a915f9140d508cf4178bf753bc3cc13b27e34560
Vlad Jerca <vlad.jerca@gmail.com>
2026-07-17 11:44:49 +0200

    stdusk 0.1.2

--- Casks/stdusk.rb
+++ Casks/stdusk.rb
@@ -1,8 +1,8 @@
 cask "stdusk" do
-  version "0.1.1"
-  sha256 "be0801d07b49b113cb0bbf6fe6b383da7f53f960754ec1633481529551ddd8cf"
+  version "0.1.2"
+  sha256 "d33e530dae734d36b4436ced637eaeaf50dde58015a330b55a8e1966cf0e7253"
 
-  url "https://github.com/Hobo-Ware/stdusk/releases/download/stdusk-v0.1.1/stdusk-0.1.1-universal.app.zip"
+  url "https://github.com/Hobo-Ware/stdusk/releases/download/stdusk-v0.1.2/stdusk-0.1.2-universal.app.zip"
   name "stdusk"
   desc "Native Rust quake terminal with a real GUI tab bar and ambient AI-CLI awareness"
   homepage "https://github.com/Hobo-Ware/stdusk"
